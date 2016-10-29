de9953872f3545b422268c746f1878e4199c1b58
Anna Chiu <awc316@nyu.edu>
2016-10-29 17:14:18 -0400

    updated Phase 4 class

--- src/main/java/edu/nyu/oop/util/MutateJavaAst.java
+++ src/main/java/edu/nyu/oop/util/MutateJavaAst.java
@@ -23,27 +23,48 @@ public class MutateJavaAst extends Visitor {
     public static GNode mutate(GNode n) {
 
         new Visitor() {
+            public void visitClassDeclaration(GNode n){
+                if(n.getNode(0) != null){
+                    if(n.getNode(0).getNode(0).getString(0).equals("public")){
+                        n.getNode(0).getNode(0).set(0,"namespace");
+                    }
+                }
+            }
+
             public void visitMethodDeclaration(GNode n) {
                 System.out.println("HIIIIIIIIIIIIIIIII Method Declarations");
 //                System.out.println(n.getString(3));
                 if (n.getString(3).equals("main")) {
                     System.out.println("HIIIIIIIIIIIIIIIII Main");
-                    n.set(0, null);
+                    GNode modifiers = GNode.create("Modifiers");
+                    n.set(0, modifiers);
                     GNode mainType = GNode.create("IntType");
                     n.set(2, mainType);
+                    GNode formalParameters = GNode.create("FormalParameters");
+                    n.set(4, formalParameters);
                 }
                 visit(n);
             }
 
-            public void visitModifiers(GNode n) {
-//                while (!n.isEmpty()) {
-//                    n.get(0);
-//                }
-//                for(int i = 0; i < n.size(); i++) {
-                    System.out.println("HIIIIIIIIIIIIIIIII Modifiers");
-//                }
+            public void visitExpressionStatement(GNode n){
+                GNode primaryIdentifier = GNode.create("PrimaryIdentifier","std");
+                GNode selectionExpressionStart = GNode.create("SelectionExpression",primaryIdentifier,"cout");
+                GNode selectionExpressionEnd = GNode.create("SelectionExpression",primaryIdentifier,"endl");
+                GNode stringLiteral = GNode.create("StringLiteral", "\"Hello.\"");
+                GNode callExpression = GNode.create("CallExpression",selectionExpressionStart,stringLiteral,selectionExpressionEnd);
+                n.set(0, callExpression);
                 visit(n);
             }
+
+//            public void visitModifiers(GNode n) {
+////                while (!n.isEmpty()) {
+////                    n.get(0);
+////                }
+////                for(int i = 0; i < n.size(); i++) {
+//                    System.out.println("HIIIIIIIIIIIIIIIII Modifiers");
+////                }
+//                visit(n);
+//            }
 
             public void visit (Node n){
                 for (Object o : n) {

992c530a2ac98fee1219a8baeebd62b4fc551d6c
Anna Chiu <awc316@nyu.edu>
2016-10-30 17:49:04 -0400

    Phase 5 Class

--- src/main/java/edu/nyu/oop/util/CPPMaker.java
+++ src/main/java/edu/nyu/oop/util/CPPMaker.java
@@ -1,7 +1,31 @@
 package edu.nyu.oop.util;
 
-/**
- * Created by AnnaChiu on 10/30/16.
- */
+import java.io.*;
+import xtc.tree.GNode;
+
+import org.slf4j.Logger;
+
 public class CPPMaker {
+    private static Logger logger = org.slf4j.LoggerFactory.getLogger(JavaFiveImportParser.class);
+
+
+    // given an ast, generate the main.cpp and output.cpp files
+    public static void printToCpp(GNode n){
+        String absPath = "";
+        File output = loadOutputCppFile(absPath);
+        try{
+            PrintWriter writer = new PrintWriter(output);
+        }
+        catch(FileNotFoundException e){
+            logger.warn("Invalid path for file " + absPath);
+        }
+    }
+
+
+    private static File loadOutputCppFile(String path){
+        File outputCpp = new File(path);
+        return outputCpp;
+    }
+
+
 }

9e805c660d064eddbdfa8082719bfcc7af0d3400
Anna Chiu <awc316@nyu.edu>
2016-10-30 17:48:28 -0400

    Updated Boot for Phase5

--- src/main/java/edu/nyu/oop/Boot.java
+++ src/main/java/edu/nyu/oop/Boot.java
@@ -1,6 +1,7 @@
 package edu.nyu.oop;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
@@ -9,8 +10,6 @@ import java.util.ArrayList; // we imported
 
 import edu.nyu.oop.util.*;
 import org.slf4j.Logger;
-
-import edu.nyu.oop.util.CPlusPlusHeaderMaker; // Me add
 
 import xtc.tree.GNode;
 import xtc.tree.Node;
@@ -50,7 +49,8 @@ public class Boot extends Tool {
         bool("printJavaImportCode", "printJavaImportCode", false, "Print Java code for imports and package source.").
         bool("generateListGNodes", "generateListGNodes", false, "Generate list of GNodes of the java class and its dependencies.").
         bool("generateHeaderOutput", "generateHeaderOutput", false, "Prints definitions into the output.h file.").
-        bool("phaseFour", "phaseFour", false, "Mutates the Java Ast files to correspond with C++ files.");
+        bool("phaseFour", "phaseFour", false, "Mutates the Java Ast files to correspond with C++ files.").
+        bool("phaseFive", "phaseFive", false, "Generates the output.cpp files and main.cpp files using mutated Asts.");
     }
 
     @Override
@@ -79,6 +79,7 @@ public class Boot extends Tool {
     }
 
     private List<GNode> listGNodes = new ArrayList<GNode>();// me add
+    private GNode mutatedAst;
     @Override
     public void process(Node n) {
         if (runtime.test("printJavaAst")) {
@@ -99,12 +100,8 @@ public class Boot extends Tool {
             runtime.console().flush();
         }
 
-//        // create the list
-//        List<GNode> g = new ArrayList<GNode>();
         // Generates list of GNodes with its dependencies
         if (runtime.test("generateListGNodes")) {
-            // create the list
-//            List<GNode> g = new ArrayList<GNode>();
             // add the GNode of the java class passed in
             g.add((GNode) n);
 
@@ -131,20 +128,16 @@ public class Boot extends Tool {
         }
         // if (runtime.test("Your command here.")) { ... don't forget to add it to init()
 
-//        GNode cppHeader = g.get(1); // phase 2 creates
-//        if(runtime.test("generateHeaderOutput")){
-//            // prints to output.h
-////            CPlusPlusHeaderMaker.printCHeaderFile(cppHeader);
-//        }
 
         if(runtime.test("phaseFour")) {
-//            MutateJavaAst m = new MutateJavaAst(g.get(0));
-//            MutateJavaAst m = new MutateJavaAst(g.get(0));
-            GNode m = MutateJavaAst.mutate(g.get(0));
-//            runtime.console().p("Mutations: " + m).pln().flush();
-            runtime.console().p("Mutate: ").format(m).pln().flush();
+            GNode nodeCopy = NodeUtil.deepCopyNode(g.get(0));
+            mutatedAst = MutateJavaAst.mutate(nodeCopy);
+//            runtime.console().p("Mutations: " + mutatedAst).pln().flush();
+            runtime.console().p("Mutate: ").format(mutatedAst).pln().flush();
+        }
 
-//            runtime.console().p("Mutate: " + m).pln().flush();
+        if(runtime.test("phaseFive")){
+            CPPMaker.printToCpp(mutatedAst);
         }
 
     }

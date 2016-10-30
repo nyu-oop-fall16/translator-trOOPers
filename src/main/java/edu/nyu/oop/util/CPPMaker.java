805e41456bcd270fcd1c04d2ddc9d83ab1e42761
Anna Chiu <awc316@nyu.edu>
2016-10-30 18:07:19 -0400

    CPP File Access Done

--- src/main/java/edu/nyu/oop/util/CPPMaker.java
+++ src/main/java/edu/nyu/oop/util/CPPMaker.java
@@ -1,6 +1,9 @@
 package edu.nyu.oop.util;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
 import xtc.tree.GNode;
 
 import org.slf4j.Logger;
@@ -11,21 +14,35 @@ public class CPPMaker {
 
     // given an ast, generate the main.cpp and output.cpp files
     public static void printToCpp(GNode n){
-        String absPath = "";
-        File output = loadOutputCppFile(absPath);
+        File output = loadOutputCpp();
         try{
-            PrintWriter writer = new PrintWriter(output);
+            PrintWriter outputWriter = new PrintWriter(output);
         }
         catch(FileNotFoundException e){
-            logger.warn("Invalid path for file " + absPath);
+            logger.warn("Invalid path for file " + output);
         }
+
+        File main = loadMainCpp();
+        try{
+            PrintWriter outputWriter = new PrintWriter(main);
+        }
+        catch(FileNotFoundException e){
+            logger.warn("Invalid path for file " + main);
+        }
+
+        System.out.println(output);
+        System.out.println(main);
     }
 
 
-    private static File loadOutputCppFile(String path){
-        File outputCpp = new File(path);
-        return outputCpp;
+    private static File loadOutputCpp() {
+        File outputCPP = new File(XtcProps.get("output.location") + "/output.cpp");
+        return outputCPP;
     }
 
+    private static File loadMainCpp() {
+        File mainCPP = new File(XtcProps.get("output.location") + "/main.cpp");
+        return mainCPP;
+    }
 
 }

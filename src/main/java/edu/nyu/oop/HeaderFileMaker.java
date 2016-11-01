ee19dbf6fed720cf45ba679fd09a62919e1e7ba8
ge490 <ge490@nyu.edu>
2016-11-01 18:43:02 -0400

    Need to do last vtable part

--- src/main/java/edu/nyu/oop/HeaderFileMaker.java
+++ src/main/java/edu/nyu/oop/HeaderFileMaker.java
@@ -10,89 +10,104 @@ import java.util.List;
 
 // this class takes in a node representing a CPP AST and translates it into a CPP Header File
 public class HeaderFileMaker {
+    File header;
+    PrintWriter writer;
 
     public void makeHeaderFile(GNode n) {
         try {
-            File header = new File("translator-trOOPers\\output\\output.h");
-            PrintWriter writer = new PrintWriter(header);
+            header = new File("translator-trOOPers\\output\\output.h");
+            writer = new PrintWriter(header);
+
+            Node head = NodeUtil.dfs(n, "Head");
+
+            // Print namespace declarations
+            List<Node> namespaces = NodeUtil.dfsAll(head, "NameSpaceDeclaration");
+            int numNameSpaces = namespaces.size();
+            for (Node ns: namespaces) {
+                writer.println("namespace " + ns.getString(0) + " {");
+            }
+
+            // Prints Declarations and TypeDefs for all Classes
+            for (Node dec: NodeUtil.dfsAll(head, "DeclarationsAndTypedef")) {
+                for (int i = 0; i < 3; i++) {
+                    writer.println(head.getString(i) + ";");
+                }
+            }
 
             List<Node> classes = NodeUtil.dfsAll(n, "ClassDeclaration");
-            List<Node> classNames = NodeUtil.dfsAll(n, "ClassName");
-            List<Node> vTableNames = NodeUtil.dfsAll(n, "VTableName");
-
-            // Struct declarations
-            for (int i = 0; i < classNames.size(); i++) {
-                writer.println("struct " + classNames.get(i).getString(0));
-                writer.println("struct " + vTableNames.get(i).getString(0));
-            }
-
-            // TypeDefs
-            for (Node c: classNames) {
-                writer.println("typedef " + c.getName() + " *" + c.getName().substring(2));
-            }
 
             // Loop through classes and print data layout and vTable for each one
-            for (int idx = 0; idx < classes.size(); idx++) {
-                Node cl = classes.get(idx);
-                writer.println("struct " + NodeUtil.dfs(cl, "ClassName").getString(0) + "{");
-                writer.println(NodeUtil.dfs(cl, "VTableName") + " *__vptr;");
+            for (Node cl: classes) {
+                String className = NodeUtil.dfs(cl, "ClassName").getString(0);
+                writer.println("struct " + className + "{");
 
-                Node clDataLayout = NodeUtil.dfs(cl, "DataLayout");
+                Node dataLayout = NodeUtil.dfs(cl, "DataLayout");
 
-                // Print constructor
-                Node constructor = NodeUtil.dfs(clDataLayout, "Constructor");
-                writer.print(NodeUtil.dfs(constructor, "ClassName") + "(");
-                List<Node> params = NodeUtil.dfsAll(constructor, "FormalParameter");
-                for (Node p: params) {
-                    writer.print(NodeUtil.dfs(p, "Qualified Identifier").getString(0) + " " + p.getString(2)); // name of parameter is not saved in name node, saved as string in FormalParameter node
+                // Prints all class field declarations
+                Node fields = NodeUtil.dfs(dataLayout, "Fields");
+                for (Node field: NodeUtil.dfsAll(fields, "FieldDeclaration")) {
+                    for (int i = 0; i < field.size(); i++) {
+                        writer.print(field.getString(0));
+                        if (i == field.size()-1) { writer.print(";\n"); }
+                    }
+                }
+
+                // Prints constructor
+                Node constructor = NodeUtil.dfs(dataLayout, "Constructor");
+                writer.print(constructor.getNode(0).getString(0)+"("); // prints name
+                for (Node p: NodeUtil.dfsAll(constructor.getNode(1), "ConstructorParameter")) {
+                    writer.print(p.getString(0) + " " + p.getString(1));
                 }
                 writer.print(");\n");
 
-                // Method Declarations
-                List<Node> methods = NodeUtil.dfsAll(clDataLayout, "DLMethodDeclaration");
+                // Method Declarations-- all declarations are modifiers returnType Name Parameters
+                Node dlMethodDecs = NodeUtil.dfs(dataLayout, "DLMethodDeclarations");
+                List<Node> methods = NodeUtil.dfsAll(dlMethodDecs, "DLMethodDeclaration");
                 for (Node method: methods) {
-                    writer.print("static " + NodeUtil.dfs(method, "ReturnType").getString(0) + " " + method.get(1) + "(");
-                    for (int i = 2; i < method.size()-1; i++) {
-                        writer.print(method.getNode(i).getString(0) + ", ");
+                    writer.print(method.getNode(0).getString(0) + " " + method.getNode(1).getString(0) + method.getNode(2).getString(0)); // print modifier "static", return type, and method name
+                    Node methodParameters = method.getNode(3);
+                    for (int i = 0; i < methodParameters.size(); i++) {
+                        writer.print(methodParameters.getString(i));
+                        if (i < methodParameters.size()-1) { writer.print(" ,"); }
+                        else { writer.print(");\n"); }
                     }
-                    writer.print(method.getNode(method.size()-1).getString(0) + ");");
-                    writer.println();
                 }
-
-                // Class object declaration (?) Idk if we need this
-                writer.println("static Class __class();");
-
-                // VTable declaration
-                writer.println("static " + vTableNames.get(idx) + " __vtable;");
 
                 writer.println("}");
 
                 //Prints VTable Method Declarations
                 Node vTable = NodeUtil.dfs(cl, "VTable");
-                writer.println("struct " + vTableNames.get(idx) + " {");
-                writer.println("Class __isa");
+                String vTableName = className + "_VT";
+
+                writer.println("struct " + vTableName + " {");
+
                 List <Node> vTableMethods = NodeUtil.dfsAll(vTable, "VTableMethodDeclaration");
                 for (Node method: vTableMethods) {
-                    writer.print(NodeUtil.dfs(method, "ReturnType").getString(0) + " (*" + method.get(1) + ")(");
-                    for (int i = 2; i < method.size()-1; i++) {
-                        writer.print(method.getNode(i).getString(0) + ", ");
+                    writer.print(method.getNode(0).getString(0) + " (*" + method.getNode(1).getString(0) + ")("); // prints returntype and pointer with name
+                    for (int i = 2; i < method.getNode(2).size(); i++) {
+                        writer.print(method.getNode(i).getString(0));
+                        if (i < method.size()-1) { writer.print(", "); }
+                        else { writer.print(");"); }
                     }
-                    writer.print(method.getNode(method.size()-1).getString(0) + ");");
                     writer.println();
                 }
 
-                writer.println(vTableNames.get(idx) + "() :");
-                writer.println("__isa(" + classNames.get(idx) + "::__class()");
+                writer.println(vTableName + "() :");
 
-                for(int m = 0; m < vTableMethods.size(); m++) {
-                    Node method = vTableMethods.get(0);
-                    writer.print(method.get(1) + "(&" + classNames.get(idx) + "::" + method.get(1) + ")");
+                // DONE THROUGH HERE
+
+                for(Node method: vTableMethods) {
+                    writer.print(method.getNode(0).getString(0) + "(&" + className + "::" + method.get(1) + ")");
                     if (m < vTableMethods.size()-1) { writer.print(","); }
                     else { writer.print("{ }"); }
                 }
                 writer.println("} ;");
             }
-            
+
+            // Put in end brackets for namespaces
+            for (int i = 0; i < numNameSpaces; i++) {
+                writer.println("}");
+            }
             writer.close();
 
         } catch (FileNotFoundException e1) {
@@ -101,5 +116,4 @@ public class HeaderFileMaker {
 
 
     }
-
 }

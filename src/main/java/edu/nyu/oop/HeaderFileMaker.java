177036b6dd9fb2fb14b3637fa5809e3761b78db6
ge490 <ge490@nyu.edu>
2016-11-05 00:16:18 -0400

    Update HeaderFileMaker.java

--- src/main/java/edu/nyu/oop/HeaderFileMaker.java
+++ src/main/java/edu/nyu/oop/HeaderFileMaker.java
@@ -1,190 +1,191 @@
 package edu.nyu.oop;
 
-import edu.nyu.oop.util.NodeUtil;
+import edu.nyu.oop.util.XtcProps;
+import org.slf4j.Logger;
 import xtc.tree.GNode;
 import xtc.tree.Node;
+import xtc.tree.Visitor;
 
 import java.io.*;
-import java.util.List;
 
+// This class visits all the nodes in a CPP AST and prints a file from it
 
-// this class takes in a node representing a CPP AST and translates it into a CPP Header File
-public class HeaderFileMaker {
-    static File header;
-    static PrintWriter writer;
+public class HeaderFileMaker extends Visitor {
+    private File header;
+    private PrintWriter writer;
 
-    public static void makeHeaderFile(GNode n) {
+    private int numNameSpaces;
+    private String className;
+    private String vTableName;
+
+    private AggregatedHeaderPrinter printer;
+
+    private Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
+
+    public void runVisitor(GNode n) { // call this from Boot.java
         try {
-            header = new File("output.h");
+            header = new File(XtcProps.get("output.location") + "/output.h");
             writer = new PrintWriter(header);
-
-            Node head = NodeUtil.dfs(n, "Head");
-
-            // Prints namespace declarations and saves number of namespaces
-            int numNameSpaces = printNamespaceDecs(head);
-
-            // Prints Declarations and TypeDefs for all Classes
-            printDecsAndTypeDefs(head);
-
-            List<Node> classes = NodeUtil.dfsAll(n, "ClassDeclaration");
-
-            // Loop through classes and print data layout and vTable for each one
-            for (Node cl: classes) {
-                String className = NodeUtil.dfs(cl, "ClassName").getString(0);
-
-                // prints beginning of class struct
-                writer.println("struct " + className + "{");
-
-                Node dataLayout = NodeUtil.dfs(cl, "DataLayoutDeclaration");
-
-                // prints data layout
-                printDataLayout(dataLayout);
-                writer.println("};");
-
-                Node vTableDec = NodeUtil.dfs(cl, "VTDeclaration");
-
-                // prints beginning of VTable struct
-                String vTableName = className + "_VT";
-                writer.println("struct " + vTableName + " {");
-
-                // prints VTable method declarations
-                printVTMethodDecs(vTableDec);
-
-                // print VTable itself
-                writer.println(vTableName + "() :");
-
-               //Node vTable = NodeUtil.dfs(vTableDec, "VTable");
-
-                //printVTable(vTable, className);
-                writer.println("} ;");
-            }
-
-            // Put in end brackets for namespaces
-            for (int i = 0; i < numNameSpaces; i++) {
-                writer.println("}");
-            }
-            writer.close();
-
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
+            printer = new AggregatedHeaderPrinter(writer);
+            visit(n);
+            printer.writeToOutputFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Header AST file was not found.");
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
     }
 
-    private static int printNamespaceDecs(Node n) {
-        List<Node> namespaces = NodeUtil.dfsAll(n, "NameSpaceDeclaration");
-        int numNameSpaces = namespaces.size();
-        for (Node ns: namespaces) {
-            writer.println("namespace " + ns.getString(0) + " {");
-        }
-        return namespaces.size();
-    }
-
-    private static void printDecsAndTypeDefs(Node n) {
-        for (Node dec: NodeUtil.dfsAll(n, "DeclarationsAndTypedef")) {
-            for (int i = 0; i < 3; i++) {
-                writer.println(dec.getString(i) + ";");
+    public void visit(GNode n) {
+        for (Object o : n) {
+            if (o instanceof Node) {
+                dispatch((Node) o);
             }
         }
     }
 
-    private static void printDataLayout(Node dataLayout) {
-        // Prints all class field declarations
-        Node fields = NodeUtil.dfs(dataLayout, "Fields");
-        if (fields != null) {
-            printClassFieldDecs(fields);
+    public void visitNameSpaceDeclaration(GNode n) {
+        numNameSpaces = n.size();
+        printer.setNumNamespaces(numNameSpaces);
+        for (int nodeIdx = 0; nodeIdx < numNameSpaces; nodeIdx++) {
+            Object nodeItem = n.get(nodeIdx);
+            if (nodeItem instanceof String) {
+                printer.addToHeader("namespace " + nodeItem + " {\n");
+            }
         }
+        visit(n);
+    }
 
-        // Prints constructor
-        Node constructor = NodeUtil.dfs(dataLayout, "ConstructorDeclaration");
-        if (constructor != null) {
-            printConstructor(constructor);
+    public void visitDeclarationsAndTypedef(GNode n) {
+        for (int i = 0; i < 3; i++) {
+            printer.addToHeader(n.getString(i) + ";\n");
         }
+        printer.addToHeader("\n");
+        visit(n);
+    }
 
-        // Method Declarations-- all declarations are modifiers returnType Name Parameters
-        Node dlMethodDecs = NodeUtil.dfs(dataLayout, "DLMethodDeclarations");
-        if (dlMethodDecs != null) {
-            printDLMethodDecs(dlMethodDecs);
+    public void visitClassDeclaration(GNode n) {
+        className = n.getNode(0).getString(0); // get Class Name
+        printer.addClass(className);
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+
+        // prints beginning of class struct
+        hcp.addToDL("struct " + className + " {\n");
+        visit(n);
+    }
+
+    public void visitFieldDeclaration(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        for (int i = 0; i < n.size(); i++) {
+            hcp.addToDL(n.getString(i));
+            if (i < n.size()-1) {
+                hcp.addToDL(" ");
+            }
+            else {
+                hcp.addToDL(";\n");
+            }
+        }
+        hcp.addToDL("\n");
+        visit(n);
+    }
+
+    public void visitConstructorDeclaration(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        hcp.addToDL(n.getNode(0).getString(0)+"("); // prints name
+        Node constParams = n.getNode(1); // get constructorParameters node
+        for (int i = 0; i < constParams.size(); i++) {
+            Node constP = constParams.getNode(i);
+            hcp.addToDL(constP.getString(0) + " " + constP.getString(1));
+        }
+        hcp.addToDL(");\n\n");
+        visit(n);
+    }
+
+    public void visitDLMethodDeclaration(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        hcp.addToDL(n.getNode(0).getString(0) + " " + n.getNode(1).getString(0) + " " + n.getNode(2).getString(0) + "("); // print modifier "static", return type, and method name
+        Node methodParameters = n.getNode(3);
+        StringBuffer s = addParamsToBuffer(methodParameters);
+        hcp.addToDL(s + ";\n");
+        visit(n);
+    }
+
+    public void visitVTDeclaration(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        hcp.addToDL("};\n\n"); // end DataLayout
+        vTableName = className + "_VT";
+        hcp.setVTableName(vTableName);
+        hcp.addToMethodDecs("struct " +  vTableName + " {\n"); // begin VTable
+        visit(n);
+    }
+
+    public void visitVTMethod(GNode n) {
+        buildMethodDecs(n);
+        buildVTable(n);
+        visit(n);
+    }
+
+    private void buildMethodDecs(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        if (n.getNode(0).getString(0).equals("isa")) {
+            hcp.addToMethodDecs(n.getNode(1).getString(0) + " " + n.getNode(0).getString(0) + ";\n");
+        }
+        else {
+            hcp.addToMethodDecs(n.getNode(1).getString(0) + " (*" + n.getNode(0).getString(0) + ")(");
+            StringBuffer s = addParamsToBuffer(n.getNode(3));
+            hcp.addToMethodDecs(s + ";\n");
         }
     }
 
-    private static void printClassFieldDecs(Node fields) {
-        for (Node field: NodeUtil.dfsAll(fields, "FieldDeclaration")) {
-            for (int i = 0; i < field.size(); i++) {
-                writer.print(field.getString(i));
-                if (i == field.size()-1) { writer.print(";\n"); }
-                else { writer.print(" "); }
+    private void buildVTable(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+
+        // if isa --> isa(__ClassName::__class())
+        if (n.getNode(0).getString(0).equals("isa")) {
+            hcp.addToVTable(n.getNode(0).getString(0) + "(" + className + "::__class()),\n");
+        }
+
+        else {
+            // if method is inherited --> methodName((ReturnType(*)(Parameters)) &__ParentObject::methodName)
+            if (!n.getNode(2).getString(0).equals(className.substring((2)))) {
+                addInheritedMethod(n);
+            }
+
+            // if not inherited --> methodName(&__thisClass::methodName)
+            else {
+                addOwnMethod(n);
             }
         }
     }
 
-    private static void printConstructor(Node constructor) {
-        writer.print(constructor.getNode(0).getString(0)+"("); // prints name
-        for (Node p: NodeUtil.dfsAll(constructor.getNode(1), "ConstructorParameter")) {
-            writer.print(p.getString(0) + " " + p.getString(1));
-        }
-        writer.print(");\n");
+    private void addInheritedMethod(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        hcp.addToVTable(n.getNode(0).getString(0) + "((" + n.getNode(1).getString(0) + "(*)(");
+        StringBuffer s = addParamsToBuffer(n.getNode(3));
+        hcp.addToVTable(s + ") &__" + n.getNode(2).getString(0) + "::" + n.getNode(0).getString(0) + "),\n");
     }
 
-    private static void printDLMethodDecs(Node dlMethodDecs) {
-        List<Node> methods = NodeUtil.dfsAll(dlMethodDecs, "DLMethodDeclaration");
-        for (Node method: methods) {
-            writer.print(method.getNode(0).getString(0) + " " + method.getNode(1).getString(0) + " " + method.getNode(2).getString(0) + "("); // print modifier "static", return type, and method name
-            Node methodParameters = method.getNode(3);
-            for (int i = 0; i < methodParameters.size(); i++) {
-                writer.print(methodParameters.getString(i));
-                if (i < methodParameters.size()-1) { writer.print(" ,"); }
-                else { writer.print(");\n"); }
-            }
-        }
+    private void addOwnMethod(GNode n) {
+        HeaderClassPrinter hcp = printer.getPrinter(className);
+        hcp.addToVTable(n.getNode(0).getString(0) + "(&" + className + "::" + n.getNode(0).getString(0) + "),\n");
     }
 
-    private static void printVTMethodDecs(Node vTableDec) {
-        Node vtMethodDecs = NodeUtil.dfs(vTableDec, "VTMethodDeclarations");
-        List <Node> vTableMethodDecs = NodeUtil.dfsAll(vtMethodDecs, "VTMethodDeclaration");
-        for (Node method: vTableMethodDecs) {
-            writer.print(method.getNode(0).getString(0) + " (*" + method.getNode(1).getString(0) + ")("); // prints returntype and pointer with name
-            Node parameters = NodeUtil.dfs(method, "Parameters");
-            for (int i = 0; i < parameters.size(); i++) {
-                writer.print(parameters.getString(i));
-                if (i < parameters.size()-1) { writer.print(", "); }
-                else { writer.print(");"); }
-            }
-            writer.println();
+    private StringBuffer addParamsToBuffer(Node params) {
+        StringBuffer s = new StringBuffer();
+        if (params.isEmpty()) {
+            s.append(")");
         }
-    }
-
-    private static void printVTable(Node vTable, String className) {
-        List<Node> vTableMethods = NodeUtil.dfsAll(vTable, "VTMethod");
-
-        for (int i = 0; i < vTableMethods.size(); i++) {
-            Node method = vTableMethods.get(i); // each one is: MethodName - Parameters - Implementation Class - ReturnType
-            String methodName = NodeUtil.dfs(method, "MethodName").getString(0);
-            writer.print(methodName + "("); // print method name
-
-            String superClassName = NodeUtil.dfs(method, "ImplementationClass").getString(0);
-            if (!superClassName.equals(className)) { // for inherited methods
-                writer.print("(" + NodeUtil.dfs(method, "ReturnType") + "(*)("); // print casting information
-                Node methodParams = NodeUtil.dfs(method, "Parameters");
-                for (int j = 0; j < methodParams.size(); j++) {
-                    if (j < methodParams.size() - 1) {
-                        writer.print(methodParams.getString(j) + ", ");
-                    } else {
-                        writer.print(methodParams.getString(j) + "))");
-                    }
+        else {
+            for (int i = 0; i < params.size(); i++) {
+                s.append(params.getString(i));
+                if (i < params.size() - 1) {
+                    s.append(", ");
+                } else {
+                    s.append(")");
                 }
-                writer.print(" &__" + superClassName);
-            } else {
-                writer.print(" &__" + className);
             }
-
-            writer.print(":: " + methodName + ")");
-
-            if (i < vTableMethods.size() - 1) {
-                writer.print(",\n");
-            } else {
-                writer.print("{\n}\n");
-            }
-
         }
+        return s;
     }
+
 }

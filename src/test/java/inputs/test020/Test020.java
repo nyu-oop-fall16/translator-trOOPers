af8f00d6a346f0cc1fdd765a26fc18ac6c714c53
Prudenceyyx <yy1308@nyu.edu>
2016-12-01 20:15:04 -0500

    c++codes for test20

--- src/test/java/inputs/test020/Test020.java
+++ src/test/java/inputs/test020/Test020.java
@@ -11,3 +11,40 @@ public class Test020 {
         System.out.println(x());
     }
 }
+
+//main.cpp
+#include <iostream>
+//#include "output.h"
+//        #include "java_lang.h"
+//
+//        using namespace std;
+//        using namespace java::lang;
+//
+//        int main(){
+//        int x;
+//        x=3;
+//        std::cout << A->x(A) << std::endl;
+//        return 0;
+//        }
+
+//output.cpp
+
+//#include "java_labg.h"
+//        #include "output.h"
+//        namespace inputs{
+//        namespace test017{
+//
+//static int __A::x(A __this){
+//        return 4;
+//        }
+//
+//
+//        Class __A::__class(){
+//static Class k = new __Class(__rt::literal("inputs.test17.A"), (Class) __Object::__class());
+//        return k;
+//        }
+//
+//        __A_VT __A::__vtable;
+//
+//        }
+//        }

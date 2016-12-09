3cfc2dbde6a66fd0c1204f2e05320e191df081dd
AnnaChiu <awc316@nyu.edu>
2016-12-08 21:22:00 -0500

    cpp commented

--- src/test/java/inputs/test015/Test015.java
+++ src/test/java/inputs/test015/Test015.java
@@ -26,3 +26,64 @@ public class Test015 {
         a.printOther(other);
     }
 }
+
+/* main.cpp
+#include <iostream>
+#include "output.h"
+#include "java_lang.h"
+
+using namespace inputs::test015;
+using namespace java::lang;
+using namespace std;
+
+int main(){
+    A a = new __A();
+    B other = new __B();
+    other->some = a;
+    a->__vptr->printOther(other);
+}
+
+*/
+
+/* output.cpp
+#include "output.h"
+#include "java_lang.h"
+
+using namespace java::lang;
+
+namespace inputs{
+    namespace test0015{
+        A::__A() : __vptr(&__vtable), some(null);
+
+        void A::printOther(A __this, A other){
+            cout << __this->__vtable->toString(other) << endl;
+        }
+
+        Class __A::__class(){
+            static Class k=new __Class(__rt::literal("java.lang.A"),(Class)__Object::__class());
+            return k;
+        }
+
+        __A_VT __A:: __vtable;
+
+
+        B::__B() : __vptr(&__vtable), some(null);
+
+        void B::printOther(B __this, A other){
+            cout << __this->__vtable->toString(other) << endl;
+        }
+
+        String B::toString(B __this){
+            return __this->some->__vptr->toString(__this);
+        }
+
+        Class __B::__class(){
+            static Class k=new __Class(__rt::literal("java.lang.B"),(Class)__A::__class());
+            return k;
+        }
+
+        __B_VT __B:: __vtable;
+    }
+}
+
+*/

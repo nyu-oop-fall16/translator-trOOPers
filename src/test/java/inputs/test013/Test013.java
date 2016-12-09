3b407ab864a5d4c480856855cfd59453dbc4e373
zamanda <az882@nyu.edu>
2016-12-08 21:26:09 -0500

    Update Test013.java

--- src/test/java/inputs/test013/Test013.java
+++ src/test/java/inputs/test013/Test013.java
@@ -20,3 +20,43 @@ public class Test013 {
         a.printOther(other);
     }
 }
+
+/*
+
+#include "output.h"
+#include "java_lang.h"
+using namespace java::lang;
+
+namespace inputs{
+    namespace test013{
+        __A::A() : __vptr(&__vtable),fld(new __String(), a){}
+
+        void __A::setA(String x, A __this){
+            __this->a = x;
+        }
+
+        void __A::printOther(A other, A __this){
+            std::cout << other->__vptr->toString(),other) << std::endl;
+        }
+    }
+}
+ */
+
+/*
+#include <iostream>
+ #include "output.h"
+ #include "java_lang.h"
+ using namespace std ;
+ using namespace java::lang;
+ using namespace inputs::test013;
+
+using namespace inputs::test013
+using namespace std
+int main(){
+A a = new __A();
+A other = a;
+other->__vptr->setA(new__String("A"),a)
+a->__vptr->printOther(other,a)
+}
+
+ */

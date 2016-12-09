962781993b6d183d5ab3e85d576d2857429582b2
AnnaChiu <awc316@nyu.edu>
2016-12-08 21:19:28 -0500

    cpp commented

--- src/test/java/inputs/test014/Test014.java
+++ src/test/java/inputs/test014/Test014.java
@@ -15,3 +15,44 @@ public class Test014 {
         a.printOther(other); // throws NullPointerException
     }
 }
+
+/* main.cpp
+#include <iostream>
+#include "output.h"
+#include "java_lang.h"
+
+using namespace inputs::test014;
+using namespace java::lang;
+using namespace std;
+
+int main(){
+    A a = new __A();
+    A other = a->some;
+    a->__vptr->printOther(a, other);
+}
+*/
+
+/* output.cpp
+#include "output.h"
+#include "java_lang.h"
+
+using namespace java::lang;
+using namespace std;
+
+namespace inputs{
+    namespace test014{
+        __A::__A() : __vptr(&__vtable){}
+
+        __A::printOther(A __this, A other){
+            cout << __this->__vptr->toString(other) << endl;
+        }
+
+        Class __A::__class(){
+            static Class k=new __Class(__rt::literal("java.lang.A"),(Class)__Object::__class());
+            return k;
+        }
+
+        __A_VT __A:: __vtable;
+    }
+}
+*/

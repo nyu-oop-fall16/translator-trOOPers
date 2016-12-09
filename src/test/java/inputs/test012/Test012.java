935b79ee25a04ce7f361691f5efa6ffc7b95d254
zamanda <az882@nyu.edu>
2016-12-08 21:25:36 -0500

    Update Test012.java

--- src/test/java/inputs/test012/Test012.java
+++ src/test/java/inputs/test012/Test012.java
@@ -48,3 +48,92 @@ public class Test012 {
         a.printOther(c);
     }
 }
+
+/*
+
+#include "output.h"
+#include "java_lang.h"
+using namespace java::lang;
+
+namespace inputs{
+    namespace test012{
+        __A::A() : __vptr(&__vtable),fld(new __String(), a){}
+
+        void __A::setA(String x, A __this){
+            __this->a = x;
+        }
+
+        void __A::printOther(A other, A __this){
+            std::cout << other->__vptr->myToString(),other) << std::endl;
+        }
+
+        String __A::myToString(A __this){
+            return __this->a;
+        }
+
+        __A_VT __A:: __vtable;
+
+        __B1::__B1() :__vptr(&__vtable),fld(new __String(),b){}
+
+        Class __B1::__class(){
+            static Class k = new __Class(__rt::literal("java.lang.B1"), (Class) __A::__class());
+            return k;
+        }
+
+        __B1_VT __B1::__vtable;
+
+
+        __B2::__B2() :__vptr(&__vtable),fld(new __String(),b){}
+
+        Class __B2::__class(){
+            static Class k = new __Class(__rt::literal("java.lang.B2"), (Class) __A::__class());
+            return k;
+        }
+
+        __B2_VT __B2::__vtable;
+
+
+        __C::__C() :__vptr(&__vtable),fld(new __String(),c){}
+
+        Class __C::__class(){
+            static Class k = new __Class(__rt::literal("java.lang.C"), (Class) __B1::__class());
+            return k;
+        }
+
+        String __C::myToString(C __this){
+            return __this-> new__String("still C");
+        }
+        __C_VT __C::__vtable;
+
+    }
+
+}
+
+
+ */
+
+
+/*
+#include <iostream>
+ #include "output.h"
+ #include "java_lang.h"
+ using namespace std ;
+ using namespace java::lang;
+ using namespace inputs::test012;
+
+int main(){
+A a = new __A();
+a->__vptr->setA(new__String("A"),a)
+B1 b1 = new __B1();
+b1->__vptr->setA(new__String("B1"),b1)
+B2 b2 = new __B2();
+b2->__vptr->setA(new__String("B2"),b2)
+C c = new __C();
+c->__vptr->setA(new__String("C"),c)
+a->__vptr->printOther(a,a)
+a->__vptr->printOther(b1,a)
+a->__vptr->printOther(b2,a)
+a->__vptr->printOther(c,a)
+}
+
+ */

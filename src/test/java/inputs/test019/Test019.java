d4efaec39162a07e24e6a2f61fab42744d2db7aa
AnnaChiu <awc316@nyu.edu>
2016-12-08 21:23:49 -0500

    cpp commented

--- src/test/java/inputs/test019/Test019.java
+++ src/test/java/inputs/test019/Test019.java
@@ -9,3 +9,38 @@ public class Test019 {
         System.out.println(x);
     }
 }
+
+/* main.cpp
+#include <iostream>
+#include "output.h"
+#include "java_lang.h"
+
+using namespace inputs::test019;
+using namespace java::lang;
+
+int main(){
+    int32_t x;
+    x = 3;
+    cout << A::x << endl;
+}
+*/
+
+/* output.cpp
+#include "output.h"
+#include "java_lang.h"
+
+using namespace java::lang;
+
+namespace inputs{
+    namespace test0019{
+        A::__A() : __vptr(&__vtable);
+
+        Class __A::__class(){
+            static Class k=new __Class(__rt::literal("java.lang.A"),(Class)__Object::__class());
+            return k;
+        }
+
+        __A_VT __A:: __vtable;
+    }
+}
+*/

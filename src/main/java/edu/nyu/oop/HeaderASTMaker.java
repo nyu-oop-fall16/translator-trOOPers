dfafbefa6cfdb4c1a84df0e577ed0223aa748731
ge490 <ge490@nyu.edu>
2016-12-16 18:11:10 -0500

    Update HeaderASTMaker.java

--- src/main/java/edu/nyu/oop/HeaderASTMaker.java
+++ src/main/java/edu/nyu/oop/HeaderASTMaker.java
@@ -5,19 +5,19 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+import edu.nyu.oop.util.NodeUtil;
+
 import xtc.tree.GNode;
 import xtc.tree.Node;
 import xtc.tree.Visitor;
 
 public class HeaderASTMaker {
-    // BuildInfo holds a list of classes that were defined in the Java file.
+    // HeaderASTMaker holds a list of classes that were defined in the Java file.
     private List<Node> packages = new ArrayList<Node>();
-    public static HashMap<String,ClassInfo> classes = new HashMap<String,ClassInfo>();
-    public String fileName;
-    public static ClassInfo object = ClassInfo.buildObject();
+    private static LinkedHashMap<String,ClassInfo> classes = new LinkedHashMap<String,ClassInfo>();
+    private String fileName;
 
-    public static LinkedHashMap <String,GNode> methodMap = new LinkedHashMap<String,GNode>();
-
+    // Makes the AST and returns the root node
     public GNode makeAST() {
         GNode completeAST = GNode.create("AST");
         GNode head = createHead();
@@ -25,172 +25,58 @@ public class HeaderASTMaker {
 
         for (String s: classes.keySet()) {
             ClassInfo c = classes.get(s);
+            mangleMethodNames(c);
 
             GNode thisClass = GNode.create("ClassDeclaration");
             GNode className = GNode.create("ClassName");
             className.add("__" + s);
             thisClass.add(className);
 
-            GNode dataLayout = createDataLayout(c);
-            GNode vTable = createVTable(c);
+            DataLayout dl = new DataLayout(c, classes);
+            VTable vt = new VTable(c, classes);
 
-            thisClass.add(dataLayout);
-            thisClass.add(vTable);
+            thisClass.add(dl.getRoot());
+            thisClass.add(vt.getRoot());
+
             completeAST.add(thisClass);
         }
         return completeAST;
     }
 
+    // Adds a given package node to the list of packages belonging to this class
     public void addPackage(Node n) {
         packages.add(n);
     }
 
-    //Creates the Data Layout for a particular ClassInfo object c.
-    private GNode createDataLayout(ClassInfo c) {
-        GNode dataLayout = GNode.create("DLDeclaration");
-
-        GNode fields = GNode.create("Fields");
-        GNode constructorDec = GNode.create("ConstructorDeclaration");
-        GNode methodDecs = GNode.create("DLMethodDeclarations");
-
-        // Fill the field declarations
-        GNode fieldVPTR = GNode.create("FieldDeclaration");
-        GNode vptrMod = GNode.create("Modifiers");
-        GNode vptrType = GNode.create("FieldType");
-        vptrType.add("__" + c.getName() + "_VT*");
-        GNode vptrName = GNode.create("FieldName");
-        vptrName.add("__vptr");
-        GNode vptrInit = GNode.create("Initialization");
-        fieldVPTR.add(vptrMod);
-        fieldVPTR.add(vptrType);
-        fieldVPTR.add(vptrName);
-        fieldVPTR.add(vptrInit);
-
-        GNode fieldVTable = GNode.create("FieldDeclaration");
-        GNode vtFieldMod = GNode.create("Modifiers");
-        vtFieldMod.add("static");
-        GNode vtFieldType = GNode.create("FieldType");
-        vtFieldType.add("__" + c.getName() + "_VT");
-        GNode vtFieldName = GNode.create("FieldName");
-        vtFieldName.add("__vtable");
-        GNode vtFieldInit = GNode.create("Initialization");
-        fieldVTable.add(vtFieldMod);
-        fieldVTable.add(vtFieldType);
-        fieldVTable.add(vtFieldName);
-        fieldVTable.add(vtFieldInit);
-
-        fields.add(fieldVPTR);
-        fields.add(fieldVTable);
-        //System.out.println(c.getFields());
-        for (GNode f: c.getFields()) {
-            GNode fieldDec = GNode.create("FieldDeclaration");
-            fieldDec = f;
-            fields.add(fieldDec);
-        }
-
-        // Fill the constructor node
-        GNode constructorName = GNode.create("ConstructorName");
-        constructorName.add("__" + c.getName());
-        constructorDec.add(constructorName);
-        constructorDec.add(c.getConstructorParams());
-
-        // Fill the declarations node
-        for (MethodInfo method: c.getMethods()) {
-            GNode newMethod = makeDLMethod(c.getName(),"static", method.getName(), method.getReturnType().getString(0), method.getParameters());
-            methodDecs.add(newMethod);
-        }
-
-        //Adding the class object
-        GNode classObject = makeDLMethod(null, "static", "__class", "Class", null);
-        methodDecs.add(classObject);
-
-        //Adding fields, constructor, and method declaration nodes to the DL node
-        dataLayout.add(fields);
-        dataLayout.add(constructorDec);
-        dataLayout.add(methodDecs);
-        return dataLayout;
+    // Gets the HashMap of classes belonging to this AST
+    public LinkedHashMap<String, ClassInfo> getClasses() {
+        return classes;
     }
 
-    //Creates the VTable for a particular ClassInfo object c.
-    private GNode createVTable(ClassInfo c) {
-        GNode vTable = GNode.create("VTDeclaration");
-        GNode vT = GNode.create("VTMethodDeclarations");
-
-        //Fill the VTMethodDeclarations node
-        makeVTMethod(c,c.getName());
-
-        //System.out.println("The completed memory map looks as follows: " + methodMap.keySet());
-        for(String s: methodMap.keySet()) {
-            vT.add(methodMap.get(s));
-        }
-        methodMap.clear();
-
-        vTable.add(vT);
-
-        return vTable;
+    // Gets a class belonging to this AST
+    public ClassInfo getClass(String className) {
+        return classes.get(className);
     }
 
-    static void makeVTMethod(ClassInfo c, String className) {
-        if (c.getParent() == null) {}
-        else if(c.getParent().equals("Object")) {
-            makeVTMethod(object, className);
-        }
-        else {
-            makeVTMethod(classes.get(c.getParent()), className);
-        }
-        for(MethodInfo m: c.getMethods()) {
-            GNode newMethod = GNode.create("VTMethod");
-
-            GNode methodName = GNode.create("MethodName");
-            GNode rType = GNode.create("ReturnType");
-            GNode implementedClass = GNode.create("ImplementedClass");
-            GNode params = GNode.create("MethodParameters");
-
-            methodName.add(m.getName());
-            rType.add(m.getReturnType().getString(0));
-            implementedClass.add(c.getName());
-            params.add(className);
-            for (String parameter : m.getParameters()) {
-                params.add(parameter);
-            }
-
-            newMethod.add(methodName);
-            newMethod.add(rType);
-            newMethod.add(implementedClass);
-            newMethod.add(params);
-
-            methodMap.put(m.getName(), newMethod);
-
-//            System.out.println("The method " + m.getName() + " has been added to " + className + " by " + c.getName());
-//            System.out.println("Now methodMap looks like: " + methodMap.keySet());
-        }
+    // Adds a class to this AST
+    public void addClass(String className, ClassInfo c) {
+        classes.put(className, c);
     }
 
-    static GNode makeDLMethod(String className, String modifier, String name, String returnType, ArrayList<String> parameters) {
-        GNode newMethod = GNode.create("DLMethodDeclaration");
+    // Adds a method to a given class belonging to the AST
+    public void addMethodToClass(String className, MethodInfo m) {
+        ClassInfo c = classes.get(className);
+        c.addMethod(m);
+    }
 
-        GNode mod = GNode.create("Modifier");
-        GNode methodName = GNode.create("MethodName");
-        GNode rType = GNode.create("ReturnType");
-        GNode params = GNode.create("Parameters");
+    // Gets the name of the file currently being worked on
+    public String getFileName() {
+        return fileName;
+    }
 
-
-        mod.add(modifier);
-        methodName.add(name);
-        rType.add(returnType);
-        if (className != null) {
-            params.add(className);
-        }
-        if(parameters != null) {
-            for (String parameter:parameters) {params.add(parameter);}
-        }
-
-        newMethod.add(mod);
-        newMethod.add(methodName);
-        newMethod.add(rType);
-        newMethod.add(params);
-
-        return newMethod;
+    // Sets the name of the file currently being worked on
+    public void setFileName(String s) {
+        fileName = s;
     }
 
     //Creates the head of the file (changing the namespace) and declaring structs for the data layouts and vtables.
@@ -214,5 +100,28 @@ public class HeaderASTMaker {
             head.add(decs);
         }
         return head;
+    }
+
+    private void mangleMethodNames(ClassInfo c) {
+        HashMap<String,Boolean> nameCheck = new HashMap<String,Boolean>();
+        for(MethodInfo m: c.getMethods()) {
+            if(!nameCheck.containsKey(m.getName())) {
+                nameCheck.put(m.getName(),false);
+            } else {
+                nameCheck.put(m.getName(),true);
+            }
+
+        }
+        for(String s: nameCheck.keySet()) {
+            if(nameCheck.get(s) == true) {
+                for(MethodInfo m: c.getMethods()) {
+                    String mangledName = m.getName();
+                    for(String st: m.getParameters()) {
+                        mangledName+= st;
+                    }
+                    m.setName(mangledName);
+                }
+            }
+        }
     }
 }

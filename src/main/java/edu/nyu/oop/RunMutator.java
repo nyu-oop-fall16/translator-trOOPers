5c1f285da5f842b71f3dee5baa8430d451a204a8
AnnaChiu <awc316@nyu.edu>
2016-12-17 18:58:32 -0500

    Create RunMutator.java

---
+++ src/main/java/edu/nyu/oop/RunMutator.java
@@ -0,0 +1,35 @@
+package edu.nyu.oop;
+
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+import xtc.tree.GNode;
+import java.util.ArrayList;
+
+public class RunMutator extends Visitor{
+    ArrayList<ClassDeclarationMutator> classes = new ArrayList<ClassDeclarationMutator>();
+    ClassDeclarationMutator main;
+
+    public void visit(Node n) {
+        for (Object o : n) if (o instanceof Node) dispatch((Node) o);
+    }
+
+    public void visitClassDeclaration(GNode n){
+        // if not main class
+        if(!n.getString(1).startsWith("Test")){
+            ClassDeclarationMutator newClass = ClassDeclarationMutator.getClassDeclarationMutator(n); // create an object
+            classes.add(newClass);
+        }
+        // else if main class
+    }
+
+    // return the list of classes and
+    public ArrayList<ClassDeclarationMutator> getClasses(Node n){
+        super.dispatch(n);
+        return classes;
+    }
+
+    public ClassDeclarationMutator getMainClass(Node n) {
+        super.dispatch(n);
+        return main;
+    }
+}

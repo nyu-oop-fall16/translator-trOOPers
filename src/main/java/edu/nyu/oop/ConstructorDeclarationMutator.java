2c9933a222a4aab06ba5282270deccbbebb89b77
AnnaChiu <awc316@nyu.edu>
2016-12-18 18:25:16 -0500

    Update ConstructorDeclarationMutator.java

--- src/main/java/edu/nyu/oop/ConstructorDeclarationMutator.java
+++ src/main/java/edu/nyu/oop/ConstructorDeclarationMutator.java
@@ -6,13 +6,13 @@ import xtc.tree.Visitor;
 
 import java.util.ArrayList;
 
-public class ConstructorDeclarationMutator extends Visitor{
+public class ConstructorDeclarationMutator extends Visitor {
     String className;
     static String returnType = "void";
     String initName;
     ArrayList<String[]> parameters = new ArrayList<String[]>();
 
-    private ConstructorDeclarationMutator(String initName, ArrayList<String[]> parameters, String className){
+    private ConstructorDeclarationMutator(String initName, ArrayList<String[]> parameters, String className) {
         this.className = className;
         this.initName = initName;
         this.parameters = parameters;
@@ -23,7 +23,7 @@ public class ConstructorDeclarationMutator extends Visitor{
      * @param n
      * @return
      */
-    public static ConstructorDeclarationMutator initSignatureInfo(GNode n, String className){
+    public static ConstructorDeclarationMutator initSignatureInfo(GNode n, String className) {
         String initName = "__" + className + "::__init";
         ArrayList<String[]> parameters = new ArrayList<String[]>();
         parameters = Mutator.getFormalParameters(n);
@@ -33,46 +33,46 @@ public class ConstructorDeclarationMutator extends Visitor{
     }
 
     /* mainly going to be used later when the implicit "this" of the class is needed. won't have the class type access here though*/
-    public void addImplicitThis(){
+    public void addImplicitThis() {
         String[] thisParameter = new String[2];
         thisParameter[0] = className;
         thisParameter[1] = "__this";
         parameters.add(0, thisParameter);
     }
 
-    public void printInitMethod(){
+    public void printInitMethod() {
         StringBuffer initImplementation = new StringBuffer();
         initImplementation.append(returnType + " " + initName + "(");
         String parametersList = "";
-        for(int p = 0; p < parameters.size(); p++){
+        for(int p = 0; p < parameters.size(); p++) {
             parametersList += (parameters.get(p)[0] + " " + parameters.get(p)[1]);
-            if(p != parameters.size()-1){
+            if(p != parameters.size()-1) {
                 parametersList += ", ";
             }
         }
         initImplementation.append(parametersList + "){\n");
-        initImplementation.append("\tBLIOCK STUFF \n}");
+        initImplementation.append("\t INIT BLOCK STUFF \n}");
         System.out.println(initImplementation);
     }
 
-    /* Assume given the node is the ConstructorDeclaration node */
-    public static void mutateConstructor(GNode n){
-        new Visitor(){
-            public void visitConstructorDeclaration(GNode n){
-                //TODO: constructor info collected
-            }
-
-            /**
-             * Dispatch to the children of a given root node.
-             * @param n the root node given
-             */
-            public void visit(Node n){
-                for (Object o : n) {
-                    if (o instanceof Node) dispatch((Node) o);
-                }
-            }
-        }.dispatch(n);
-    }
+//    /* Assume given the node is the ConstructorDeclaration node */
+//    public static void mutateConstructor(GNode n) {
+//        new Visitor() {
+//            public void visitConstructorDeclaration(GNode n) {
+//                //TODO: constructor info collected
+//            }
+//
+//            /**
+//             * Dispatch to the children of a given root node.
+//             * @param n the root node given
+//             */
+//            public void visit(Node n) {
+//                for (Object o : n) {
+//                    if (o instanceof Node) dispatch((Node) o);
+//                }
+//            }
+//        } .dispatch(n);
+//    }
 
     //TODO: implement init() methods
 }

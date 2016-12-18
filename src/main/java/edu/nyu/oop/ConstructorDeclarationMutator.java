bb9d1789ccc04e3661e7d8393082287c08866d15
AnnaChiu <awc316@nyu.edu>
2016-12-18 02:10:24 -0500

    Update ConstructorDeclarationMutator.java

--- src/main/java/edu/nyu/oop/ConstructorDeclarationMutator.java
+++ src/main/java/edu/nyu/oop/ConstructorDeclarationMutator.java
@@ -4,12 +4,58 @@ import xtc.tree.GNode;
 import xtc.tree.Node;
 import xtc.tree.Visitor;
 
-public class ConstructorDeclarationMutator {
-    String constructorCall; // ex __A::__A() for call to constructor A
+import java.util.ArrayList;
 
-    /*
-        Assume given the node is the MethodDeclaration node
-    */
+public class ConstructorDeclarationMutator extends Visitor{
+    String className;
+    static String returnType = "void";
+    String initName;
+    ArrayList<String[]> parameters = new ArrayList<String[]>();
+
+    private ConstructorDeclarationMutator(String initName, ArrayList<String[]> parameters, String className){
+        this.className = className;
+        this.initName = initName;
+        this.parameters = parameters;
+    }
+
+    /**
+     * Creates a ConstructionDeclarationMutator object with the signature info of the
+     * @param n
+     * @return
+     */
+    public static ConstructorDeclarationMutator initSignatureInfo(GNode n, String className){
+        String initName = "__" + className + "::__init";
+        ArrayList<String[]> parameters = new ArrayList<String[]>();
+        parameters = Mutator.getFormalParameters(n);
+
+        ConstructorDeclarationMutator newInit = new ConstructorDeclarationMutator(initName, parameters, className);
+        return newInit;
+    }
+
+    /* mainly going to be used later when the implicit "this" of the class is needed. won't have the class type access here though*/
+    public void addImplicitThis(){
+        String[] thisParameter = new String[2];
+        thisParameter[0] = className;
+        thisParameter[1] = "__this";
+        parameters.add(0, thisParameter);
+    }
+
+    public void printInitMethod(){
+        StringBuffer initImplementation = new StringBuffer();
+        initImplementation.append(returnType + " " + initName + "(");
+        String parametersList = "";
+        for(int p = 0; p < parameters.size(); p++){
+            parametersList += (parameters.get(p)[0] + " " + parameters.get(p)[1]);
+            if(p != parameters.size()-1){
+                parametersList += ", ";
+            }
+        }
+        initImplementation.append(parametersList + "){\n");
+        initImplementation.append("\tBLIOCK STUFF \n}");
+        System.out.println(initImplementation);
+    }
+
+    /* Assume given the node is the ConstructorDeclaration node */
     public static void mutateConstructor(GNode n){
         new Visitor(){
             public void visitConstructorDeclaration(GNode n){
@@ -26,13 +72,6 @@ public class ConstructorDeclarationMutator {
                 }
             }
         }.dispatch(n);
-    }
-
-    /**
-     * Replace current node with one we create - simple nodes
-     */
-    public static void recreateConstructorNode(){
-
     }
 
     //TODO: implement init() methods

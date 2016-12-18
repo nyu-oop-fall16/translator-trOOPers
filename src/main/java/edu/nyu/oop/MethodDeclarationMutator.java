a265c25dd2140a951685a6ecc3bf280c655d8d02
AnnaChiu <awc316@nyu.edu>
2016-12-18 01:02:23 -0500

    Update MethodDeclarationMutator.java

--- src/main/java/edu/nyu/oop/MethodDeclarationMutator.java
+++ src/main/java/edu/nyu/oop/MethodDeclarationMutator.java
@@ -9,18 +9,23 @@ public class MethodDeclarationMutator extends Visitor{
     String returnType;
     String methodName;
     ArrayList<String[]> parameters;
+    String className;
 
-    private MethodDeclarationMutator(String returnType, String methodName, ArrayList<String[]> parameters){
+    private MethodDeclarationMutator(String returnType, String methodName, ArrayList<String[]> parameters, String className){
         this.returnType = returnType;
         this.methodName = methodName;
         this.parameters = parameters;
+        this.className = className;
     }
 
     /*
         mainly going to be used later when the implicit "this" of the class is needed. won't have the class type access here though
      */
-    public void addToParameters(String[] newParameter){
-        this.parameters.add(newParameter);
+    public void addImplicitThis(){
+        String[] thisParameter = new String[2];
+        thisParameter[0] = className;
+        thisParameter[1] = "__this";
+        parameters.add(0, thisParameter);
     }
 
     /**
@@ -28,14 +33,14 @@ public class MethodDeclarationMutator extends Visitor{
      * @param n
      * @return
      */
-    public static MethodDeclarationMutator methodSignatureInfo(GNode n){
+    public static MethodDeclarationMutator methodSignatureInfo(GNode n, String className){
         // if VoidType then return "void", otherwise get the name of the return type
-        String returnType = (n.getTokenText() == "VoidType") ? "void":n.getNode(2).getNode(0).getString(0);
+        String returnType = (n.getNode(2).getName() == "VoidType") ? "void":n.getNode(2).getNode(0).getString(0);
         String methodName = n.getString(3);
         ArrayList<String[]> parameters = new ArrayList<String[]>();
         parameters = Mutator.getFormalParameters(n);
 
-        MethodDeclarationMutator newMethod = new MethodDeclarationMutator(returnType, methodName, parameters);
+        MethodDeclarationMutator newMethod = new MethodDeclarationMutator(returnType, methodName, parameters, className);
         return newMethod;
     }
 
@@ -58,5 +63,22 @@ public class MethodDeclarationMutator extends Visitor{
                 }
             }
         }.dispatch(n);
+    }
+
+    public void printMethodImplementation(){
+        StringBuffer methodImplementation = new StringBuffer();
+        methodImplementation.append(returnType + " ");
+        String classSignature = "__" + className + "::" + methodName + "(";
+        methodImplementation.append(classSignature);
+        String parametersList = "";
+        for(int p = 0; p < parameters.size(); p++){
+            parametersList += (parameters.get(p)[0] + " " + parameters.get(p)[1]);
+            if(p != parameters.size()-1){
+                parametersList += ", ";
+            }
+        }
+        methodImplementation.append(parametersList + "){\n");
+        methodImplementation.append("\tBLIOCK STUFF \n}");
+        System.out.println(methodImplementation);
     }
 }

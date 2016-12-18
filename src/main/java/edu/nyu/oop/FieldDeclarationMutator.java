3b810b38219299af28695a0dfbce78be7c5576b7
AnnaChiu <awc316@nyu.edu>
2016-12-18 18:28:20 -0500

    Update FieldDeclarationMutator.java

--- src/main/java/edu/nyu/oop/FieldDeclarationMutator.java
+++ src/main/java/edu/nyu/oop/FieldDeclarationMutator.java
@@ -1,50 +1,35 @@
 package edu.nyu.oop;
 
-import edu.nyu.oop.util.RecursiveVisitor;
-import xtc.tree.Node;
-import xtc.tree.Visitor;
 import xtc.tree.GNode;
-import java.util.ArrayList;
 
-public class FieldDeclarationMutator{
-    String[] fieldMember;
-    String[] mainFieldMember;
+public class FieldDeclarationMutator {
+    static String[] fieldMember;
 
-    private FieldDeclarationMutator(String[] fieldMember, String[] mainFieldMember){
+    private FieldDeclarationMutator(String[] fieldMember) {
         this.fieldMember = fieldMember;
-        this.mainFieldMember = mainFieldMember;
     }
 
-    public static FieldDeclarationMutator getClassField(GNode n){
-        FieldDeclarationMutator newField = classFieldMembers(n);
-        return newField;
-    }
-
-    public static FieldDeclarationMutator getMainField(GNode n){
-        FieldDeclarationMutator newField = mainFieldMembers(n);
-        return newField;
-    }
-
-
-    // visit FieldDeclaration node
-    public static FieldDeclarationMutator classFieldMembers(GNode n){
+    // visit FieldDeclaration node for regular classes
+    public static FieldDeclarationMutator classFieldMembers(GNode n) {
+        System.out.println("CLASS FIELD MEMBER");
         String[] field = new String[2];
         field[0] = n.getNode(1).getNode(0).getString(0); // gets the type
         field[1] = n.getNode(2).getNode(0).getString(0); // gets the name of the field
-        String[] mainField = new String[3];
 
-        FieldDeclarationMutator newField = new FieldDeclarationMutator(field, mainField);
+        FieldDeclarationMutator newField = new FieldDeclarationMutator(field);
         return newField;
     }
 
-   public static FieldDeclarationMutator mainFieldMembers(GNode n){
-       String[] field = new String[3];
-       field[0] = n.getNode(0).getNode(0).getString(0); // gets modifier
-       field[1] = n.getNode(1).getNode(0).getString(0); // gets the type
-       field[2] = n.getNode(2).getNode(0).getString(0); // gets the name of the field
-       String[] classField = new String[2];
+    // visit FieldDeclaration node for main class
+    public static FieldDeclarationMutator mainFieldMembers(GNode n) {
+        System.out.println("MAIN FIELD MEMBER");
+        String[] field = new String[3];
+        field[0] = n.getNode(0).getNode(0).getString(0); // gets modifier
+        field[1] = n.getNode(1).getNode(0).getString(0); // gets the type
+        field[2] = n.getNode(2).getNode(0).getString(0); // gets the name of the field
+        String[] classField = new String[2];
 
-       FieldDeclarationMutator newField = new FieldDeclarationMutator(classField, field);
-       return newField;
-   }
+        FieldDeclarationMutator newField = new FieldDeclarationMutator(classField);
+        return newField;
+    }
 }

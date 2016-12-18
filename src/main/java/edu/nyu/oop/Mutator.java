6d93e4fe3c53535c555f0f300b41396b605bc2ce
AnnaChiu <awc316@nyu.edu>
2016-12-18 18:23:18 -0500

    Update Mutator.java

--- src/main/java/edu/nyu/oop/Mutator.java
+++ src/main/java/edu/nyu/oop/Mutator.java
@@ -3,22 +3,21 @@ package edu.nyu.oop;
 import xtc.tree.GNode;
 import xtc.tree.Node;
 import xtc.tree.Visitor;
-
 import java.util.ArrayList;
 
 /*
    Each method should be a call to a visit method BUT, in each visit, don't call visit() so that the action is
    done locally in the current node alone. In the classes that call methods of this class, a call to visit is made.
  */
-public class Mutator{
+public class Mutator {
     /**
      * Method to visit and mutate ClassDeclaration nodes. Assume it receives a ClassDeclaration node.
      * @param n
      */
     public static void mutateClassDeclaration(GNode n) {
-        new Visitor(){
+        new Visitor() {
 
-        }.dispatch(n);
+        } .dispatch(n);
     }
 
     /**
@@ -26,9 +25,9 @@ public class Mutator{
      * @param n
      */
     public static void mutateClassBody(GNode n) {
-        new Visitor(){
+        new Visitor() {
 
-        }.dispatch(n);
+        } .dispatch(n);
     }
 
     /**
@@ -36,31 +35,31 @@ public class Mutator{
      * @param n the given GNode (usually the MethodDeclaration)
      * @return an array list of String arrays containing the type and name of a parameter
      */
-    public static ArrayList<String[]> getFormalParameters(GNode n){
+    public static ArrayList<String[]> getFormalParameters(GNode n) {
         final ArrayList<String[]> parameters = new ArrayList<String[]>();
 
-        new Visitor(){
+        new Visitor() {
             String[] parameter = new String[2];
             /**
              * Visit FormalParameters and call visit() on the node to have other visit methods called.
              * @param n the root node given to be visited
              */
-            public void visitFormalParameters(GNode n){
+            public void visitFormalParameters(GNode n) {
                 visit(n);
             }
 
-            public void visitFormalParameter(GNode n){
+            public void visitFormalParameter(GNode n) {
                 parameter = new String[2]; // reset
                 visit(n); // call visit to get the type first
                 parameter[1] = n.getString(3);
                 parameters.add(parameter);
             }
 
-            public void visitType(GNode n){
+            public void visitType(GNode n) {
                 visit(n);
             }
 
-            public void visitQualifiedIdentifier(GNode n){
+            public void visitQualifiedIdentifier(GNode n) {
                 parameter[0] = n.getString(0);
             }
 
@@ -68,12 +67,12 @@ public class Mutator{
              * Dispatch to the children of a given root node.
              * @param n the root node given
              */
-            public void visit(Node n){
+            public void visit(Node n) {
                 for (Object o : n) {
                     if (o instanceof Node) dispatch((Node) o);
                 }
             }
-        }.dispatch(n);
+        } .dispatch(n);
 
         return parameters;
     }

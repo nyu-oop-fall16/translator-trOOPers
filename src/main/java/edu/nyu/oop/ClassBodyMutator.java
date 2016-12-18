dc448b1ecfbbb9026159fa97f70f201fc00379d4
AnnaChiu <awc316@nyu.edu>
2016-12-17 22:12:28 -0500

    Update ClassBodyMutator.java

--- src/main/java/edu/nyu/oop/ClassBodyMutator.java
+++ src/main/java/edu/nyu/oop/ClassBodyMutator.java
@@ -11,6 +11,7 @@ public class ClassBodyMutator extends Visitor {
     String className;
     ArrayList<ConstructorDeclarationMutator> constructors;
     ArrayList<MethodDeclarationMutator> methods;
+    ArrayList<FieldDeclarationMutator> fields;
 
     /**
      * Constructs an object of ClassBodyMutator which holds the class's name and its class body.
@@ -18,27 +19,27 @@ public class ClassBodyMutator extends Visitor {
     private ClassBodyMutator(){
         this.constructors = new ArrayList<ConstructorDeclarationMutator>();
         this.methods = new ArrayList<MethodDeclarationMutator>();
+        this.fields = new ArrayList<FieldDeclarationMutator>();
     }
 
     public static ClassBodyMutator classMethodAndConstructor(GNode n, String className){
         ClassBodyMutator newClass = new ClassBodyMutator();
 
         newClass.className = className;
-
-        ArrayList<MethodDeclarationMutator> methodList = newClass.methods;
-        newClass.handleMethods(n, methodList); // handle methods
-
-        ArrayList<ConstructorDeclarationMutator> constructorList = newClass.constructors;
-        newClass.handleConstructors(n, constructorList); // handle constructors
+        newClass.handleMethods(n); // handle methods
+        newClass.handleConstructors(n); // handle constructors
+        newClass.handleFields(n); // handle fields
 
         return newClass;
     }
 
-    public void handleMethods(GNode n, final ArrayList<MethodDeclarationMutator> methods){
+    public void handleMethods(GNode n){
         new Visitor(){
             public void visitMethodDeclaration(GNode n) {
                 methods.add(MethodDeclarationMutator.methodSignatureInfo(n)); // add each new method signature to method list
                 MethodDeclarationMutator.mutateMethod(n); // call method to mutate the node to reflect C++
+
+                visit(n);
             }
 
             /**
@@ -53,10 +54,29 @@ public class ClassBodyMutator extends Visitor {
         }.dispatch(n);
     }
 
-    public void handleConstructors(GNode n, final ArrayList<ConstructorDeclarationMutator> constructors){
+    public void handleConstructors(GNode n){
         new Visitor(){
             public void visitConstructorDeclaration(GNode n) {
                 // add each new constructor to constructor list
+
+            }
+
+            /**
+             * Dispatch to the children of a given root node.
+             * @param n the root node given
+             */
+            public void visit(Node n){
+                for (Object o : n) {
+                    if (o instanceof Node) dispatch((Node) o);
+                }
+            }
+        }.dispatch(n);
+    }
+
+    public void handleFields(GNode n){
+        new Visitor(){
+            public void visitFieldDeclaration(GNode n) {
+                // add each new field to feilds list
 
             }
 

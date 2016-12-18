99907adbf36504db995dcc56d0e45fa4d08b67d7
AnnaChiu <awc316@nyu.edu>
2016-12-18 18:24:09 -0500

    Update ClassDeclarationMutator.java

--- src/main/java/edu/nyu/oop/ClassDeclarationMutator.java
+++ src/main/java/edu/nyu/oop/ClassDeclarationMutator.java
@@ -10,46 +10,53 @@ public class ClassDeclarationMutator extends Visitor {
     String classExtension;
     ClassBodyMutator classBody;
     ChildToParentMap map;
+    boolean isMain;
 
     /**
      * Constructs an object of ClassDeclarationMutator which holds the class's name and its class body.
      * @param n a ClassDeclaration node
      */
-    private ClassDeclarationMutator(GNode n, ChildToParentMap map){
+    private ClassDeclarationMutator(GNode n, ChildToParentMap map, boolean isMainClass){
         this.map = map;
         this.className = n.getString(1);
         this.classExtension = (n.get(3) == null) ? null:n.getNode(3).getNode(0).getNode(0).getString(0);
-        super.dispatch(n); // dispatch to run visit to ClassBody node and set classBody to results
+        this.isMain = isMainClass; //  set true if class is main class
+        super.dispatch(n); // dispatch to run visit to ClassBody node and set classBody to result
     }
 
     // create a new instance of class
-    public static ClassDeclarationMutator getClassDeclarationMutator(GNode n, ChildToParentMap map){
-        ClassDeclarationMutator newClass = new ClassDeclarationMutator(n,map);
+    public static ClassDeclarationMutator getClassDeclarationMutator(GNode n, ChildToParentMap map, boolean isMain) {
+        ClassDeclarationMutator newClass = new ClassDeclarationMutator(n, map, isMain);
         return newClass;
     }
 
     // visit class body and create a new instance of class body
-    public void visitClassBody(GNode n){
-        // call ClassBodyMutator
-        this.classBody = ClassBodyMutator.classMethodAndConstructor(n, this.className, this.classExtension, map);
+    public void visitClassBody(GNode n) {
+        if (!isMain) {
+            this.classBody = ClassBodyMutator.regularClass(n, this.className, this.classExtension, map, isMain);
+        }
+        else{
+            this.classBody = ClassBodyMutator.mainClass(n, map, isMain);
+        }
     }
 
     /**
      * Dispatch to the children of a given root node.
      * @param n the root node given
      */
-    public void visit(Node n){
+    public void visit(Node n) {
         for (Object o : n) {
             if (o instanceof Node) dispatch((Node) o);
         }
     }
 
-    public void printConstructor(){
+    // prints the constructor for the class
+    public void printConstructor() {
         System.out.println(classBody.constructor);
     }
 
     // prints the implementation of the __class() of each class
-    public void printClassMethod(){
+    public void printClassMethod() {
         StringBuffer classImplementation = new StringBuffer();
         classImplementation.append("Class ");
         String methodCall = "__" + className + "::__class(){";
@@ -64,7 +71,7 @@ public class ClassDeclarationMutator extends Visitor {
     }
 
     // prints the implementation of vtable of each class
-    public void printVTable(){
+    public void printVTable() {
         StringBuffer vtable = new StringBuffer();
         String vt = "__" + className + "_VT ";
         vtable.append(vt);
@@ -72,5 +79,4 @@ public class ClassDeclarationMutator extends Visitor {
         vtable.append(vtCall);
         System.out.println(vtable);
     }
-
 }

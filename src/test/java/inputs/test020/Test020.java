8d875dac41161c29dab0a4302d6bcecfee413a94
zamanda <az882@nyu.edu>
2016-12-19 13:31:37 -0500

    Update Test020.java

--- src/test/java/inputs/test020/Test020.java
+++ src/test/java/inputs/test020/Test020.java
@@ -7,11 +7,10 @@ class A {
     }
 }
 
-package inputs.test020;
-
-class A {
-    static int x;
-    static int x() {
-        return 4;
+public class Test020 {
+    public static void main(String[] args) {
+        int x;
+        x = A.x;
+        System.out.println(A.x());
     }
 }
